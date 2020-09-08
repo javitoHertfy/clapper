6b87c4313760ae2e29c10c3c5e1f8277370aae95
javierhertfelder <javierhertfelder@gmail.com>
2020-09-08 07:37:34 +0200

    First commit working main.go!

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+"fmt"
+"log"
+"net/http"
+)
+
+func homePage (w http.ResponseWriter, r *http.Request)  {
+
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+    fmt.Println("Endpoint Hit: homePage")
+	
+}
+
+func handleRequest(){
+
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8081",nil))
+
+}
+func main(){
+	handleRequest()
+}
